f2d61f7e31244c6027d0c5432d2e6faefa7a7b3d
niku <niku.gronberg@develore.com>
2026-01-14 13:22:38 +0200

    day 2 solution 2

--- day-2/Solver.cs
+++ day-2/Solver.cs
@@ -32,6 +32,56 @@ public class Solver
         return sum;
     }
 
+    public long Solve2(List<string> input)
+    {
+        var ranges = ParseRanges(input[0]);
+
+        long sum = 0;
+
+        foreach (var range in ranges)
+        {
+            var startNumber = long.Parse(range.Start);
+            var endNumber = long.Parse(range.End);
+
+            for (var i = startNumber; i <= endNumber; i++)
+            {
+                var value = i.ToString().AsSpan();
+                var length = value.Length;
+
+                // For 12345 half is 2, since 3 long sequence is too long to repeat
+                // so integer division works
+                var half = length / 2;
+
+                for (var sequenceLength = 1; sequenceLength <= half; sequenceLength++)
+                {
+                    if (length % sequenceLength != 0) continue;
+
+                    var sequence = value[0..sequenceLength];
+
+                    var isInvalid = true;
+                    for (var window = sequenceLength; window < length; window += sequenceLength)
+                    {
+                        if (!MemoryExtensions.SequenceEqual(sequence, value[window..(window + sequenceLength)]))
+                        {
+                            isInvalid = false;
+                            break;
+                        }
+                    }
+
+                    if (isInvalid)
+                    {
+                        // Console.WriteLine($"Invalid value {i}, invalid sequence {sequence}");
+                        sum += i;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        return sum;
+    }
+
     private List<Range> ParseRanges(string input)
     {
         List<Range> results = [];
