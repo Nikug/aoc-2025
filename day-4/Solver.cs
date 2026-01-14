e41e44c6cfd7a9fe5b7c78925202f37a5579168e
niku <niku.gronberg@develore.com>
2026-01-14 14:49:08 +0200

    day 4 solution 2

--- day-4/Solver.cs
+++ day-4/Solver.cs
@@ -61,6 +61,60 @@ public class Solver
         return result;
     }
 
+    public long Solve2(List<string> input)
+    {
+        var size = new Vector2(input[0].Length, input.Count);
+        var maxSurroundingObjects = 3;
+        var removedCount = 0;
+        var removed = new HashSet<Vector2>();
+
+        do
+        {
+            removedCount = 0;
+            for (var y = 0; y < size.Y; y++)
+            {
+                for (var x = 0; x < size.X; x++)
+                {
+                    var position = new Vector2(x, y);
+                    var positionCharacter = input[position.Y][position.X];
+                    if (positionCharacter != '@' || removed.Contains(position))
+                    {
+                        continue;
+                    }
+
+                    var surroundingObjects = 0;
+                    foreach (var direction in directions)
+                    {
+                        var newPosition = position + direction;
+                        if (outOfBounds(newPosition, size) || removed.Contains(newPosition))
+                        {
+                            continue;
+                        }
+
+                        var character = input[newPosition.Y][newPosition.X];
+                        if (character == '@')
+                        {
+                            surroundingObjects += 1;
+                            if (surroundingObjects > maxSurroundingObjects)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (surroundingObjects <= maxSurroundingObjects)
+                    {
+                        // Console.WriteLine($"Valid position {position}");
+                        removed.Add(position);
+                        removedCount += 1;
+                    }
+                }
+            }
+        } while (removedCount > 0);
+
+        return removed.Count;
+    }
+
     private bool outOfBounds(Vector2 position, Vector2 limits)
     {
         return position.X < 0 || position.X >= limits.X || position.Y < 0 || position.Y >= limits.Y;
