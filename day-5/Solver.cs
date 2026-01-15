e40e5957b778eb66a0700e3b690750eb2d830117
niku <niku.gronberg@develore.com>
2026-01-15 09:49:34 +0200

    day 5 solution 2

--- day-5/Solver.cs
+++ day-5/Solver.cs
@@ -23,6 +23,44 @@ public class Solver
         return sum;
     }
 
+    public long Solve2(List<string> input)
+    {
+        var (ranges, _) = ParseValues(input);
+
+        // Sort ascending by start
+        ranges.Sort((Range a, Range b) =>
+        {
+            if (a.Start > b.Start) return 1;
+            if (a.Start < b.Start) return -1;
+            return 0;
+        });
+
+        for (var i = 0; i < ranges.Count - 1; i++)
+        {
+            var current = ranges[i];
+            var next = ranges[i + 1];
+            // Console.WriteLine($"Checking: {current} {next}");
+
+            if (current.End >= next.Start)
+            {
+                ranges[i] = new Range(current.Start, Math.Max(current.End, next.End));
+                ranges.RemoveAt(i + 1);
+                // Check the new combined range against the new next in the list
+                i -= 1;
+            }
+        }
+
+
+        long sum = 0;
+        foreach (var range in ranges)
+        {
+            // Console.WriteLine($"Range: {range}, size: {range.Count()}");
+            sum += range.Count();
+        }
+
+        return sum;
+    }
+
     private (List<Range> ranges, List<long> values) ParseValues(List<string> input)
     {
         List<Range> ranges = [];
@@ -51,4 +89,8 @@ public class Solver
     }
 }
 
-public record Range(long Start, long End);
+public record Range(long Start, long End)
+{
+    public override string ToString() => $"{this.Start}-{this.End}";
+    public long Count() => this.End - this.Start + 1;
+};
