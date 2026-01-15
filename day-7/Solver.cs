c35b82fc98afc680903485865cdefdfec6a39be0
niku <niku.gronberg@develore.com>
2026-01-15 14:12:49 +0200

    day 7 solution 2

--- day-7/Solver.cs
+++ day-7/Solver.cs
@@ -56,9 +56,105 @@ public class Solver
         return splitters.Count;
     }
 
+    public long Solve2(List<string> input)
+    {
+        var bounds = new Vector2(input[0].Length, input.Count);
+
+        Dictionary<Vector2, long> beams = [];
+        Dictionary<Vector2, long> splitters = [];
+        for (var y = 0; y < bounds.Y; y++)
+        {
+            for (var x = 0; x < bounds.X; x++)
+            {
+                var character = input[y][x];
+                var position = new Vector2(x, y);
+
+                var positionAbove = position + directions.Up;
+                if (character == 'S')
+                {
+                    var positionBelow = position + directions.Down;
+                    beams.Add(positionBelow, 1);
+                    break;
+                }
+                else if (character == '^' && beams.ContainsKey(positionAbove))
+                {
+                    var numberOfBeams = beams[positionAbove];
+                    splitters.Add(position, numberOfBeams);
+                    var positionLeft = position + directions.Left;
+                    var positionRight = position + directions.Right;
+
+                    if (beams.ContainsKey(positionLeft))
+                    {
+                        beams[positionLeft] += numberOfBeams;
+                    }
+                    else
+                    {
+                        beams.Add(positionLeft, numberOfBeams);
+                    }
+
+                    if (beams.ContainsKey(positionRight))
+                    {
+                        beams[positionRight] += numberOfBeams;
+                    }
+                    else
+                    {
+                        beams.Add(positionRight, numberOfBeams);
+                    }
+                }
+                else if (character == '.' && beams.ContainsKey(positionAbove))
+                {
+                    if (beams.ContainsKey(position))
+                    {
+                        beams[position] += beams[positionAbove];
+                    }
+                    else
+                    {
+                        beams.Add(position, beams[positionAbove]);
+                    }
+                }
+            }
+        }
+
+        // Console.WriteLine("Beams: " + string.Join(", ", beams));
+        // Console.WriteLine("Splitters: " + string.Join(", ", splitters));
+        // PrintBeams(input, beams);
+
+        long sum = 0;
+        var beamEnds = beams.Where(entry => entry.Key.Y == bounds.Y - 1);
+        foreach (var (_, value) in beamEnds)
+        {
+            sum += value;
+        }
+
+        return sum;
+    }
+
     private bool OutOfBounds(Vector2 position, Vector2 bounds)
     {
         return position.X < 0 || position.X >= bounds.X || position.Y < 0 || position.Y >= bounds.Y;
+    }
+
+    private void PrintBeams(List<string> input, Dictionary<Vector2, long> beams)
+    {
+        var bounds = new Vector2(input[0].Length, input.Count);
+
+        for (var y = 0; y < bounds.Y; y++)
+        {
+            var line = string.Empty;
+            for (var x = 0; x < bounds.X; x++)
+            {
+                var position = new Vector2(x, y);
+                if (beams.ContainsKey(position))
+                {
+                    line += beams[position];
+                }
+                else
+                {
+                    line += input[y][x];
+                }
+            }
+            Console.WriteLine(line);
+        }
     }
 
 }
