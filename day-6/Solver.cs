564fe1efff603d19876639b91b8553aaec49aeee
niku <niku.gronberg@develore.com>
2026-01-15 10:34:34 +0200

    day 6 solution 2

--- day-6/Solver.cs
+++ day-6/Solver.cs
@@ -17,6 +17,19 @@ public class Solver
         return sum;
     }
 
+    public long Solve2(List<string> input)
+    {
+        var calculations = ParseCalculationsVertical(input);
+
+        long sum = 0;
+        foreach (var calculation in calculations)
+        {
+            // Console.WriteLine(calculation);
+            sum += calculation.Calculate();
+        }
+        return sum;
+    }
+
     private List<Calculation> ParseCalculations(List<string> input)
     {
         var pattern = @"\s+";
@@ -53,6 +66,51 @@ public class Solver
                 }
             }
         }
+
+        return calculations;
+    }
+
+    private List<Calculation> ParseCalculationsVertical(List<string> input)
+    {
+        var width = input[0].Length;
+        var height = input.Count;
+        List<Calculation> calculations = [];
+
+        var calculation = new Calculation { Values = [], Operation = '+' };
+        for (var x = width - 1; x >= 0; x--)
+        {
+            var buffer = string.Empty;
+            for (var y = 0; y < height; y++)
+            {
+                var character = input[y][x];
+                if (character == ' ')
+                {
+                    continue;
+                }
+                else if (character == '+' || character == '*')
+                {
+                    calculation.Operation = character;
+                }
+                else
+                {
+                    buffer += character;
+                }
+            }
+
+            if (buffer.Length > 0)
+            {
+                calculation.Values.Add(long.Parse(buffer));
+            }
+            else
+            {
+                // Empty column, calculation is done
+                calculations.Add(calculation);
+                calculation = new Calculation { Values = [], Operation = '+' };
+            }
+        }
+
+        // Data doesn't start with empty column, add last calculation manually
+        calculations.Add(calculation);
 
         return calculations;
     }
