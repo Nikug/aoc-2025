a2a8cc7422173e509f825eb0e45e2973fe70a43d
niku <niku.gronberg@develore.com>
2026-01-22 12:34:32 +0200

    day 8 solution 2

--- day-8/Solver.cs
+++ day-8/Solver.cs
@@ -97,6 +97,93 @@ public class Solver
         return result;
     }
 
+    public long Solve2(List<string> input)
+    {
+        var boxes = ParseJunctionBoxes(input);
+        PriorityQueue<DistancePair, double> pairs = new();
+
+        for (var i = 0; i < boxes.Count - 1; i++)
+        {
+            var current = boxes[i];
+            for (var j = i + 1; j < boxes.Count; j++)
+            {
+                var next = boxes[j];
+                var distance = Distance(current, next);
+                pairs.Enqueue(new DistancePair(current, next, distance), distance);
+            }
+        }
+
+        List<Circuit> circuits = [];
+        foreach (var box in boxes)
+        {
+            circuits.Add(new Circuit() { Nodes = [box] });
+        }
+
+        Connection? latestConnection = null;
+        while (circuits.Count > 1)
+        {
+            var minPair = pairs.Dequeue();
+            var newConnection = new Connection(minPair.First, minPair.Second);
+            // Console.WriteLine($"Round {i}: Checking connection {minPair.First} {minPair.Second}");
+
+            var circuitsWithFirst = circuits.Where(circuit => circuit.Nodes.Contains(minPair.First)).ToArray();
+            var circuitsWithSecond = circuits.Where(circuit => circuit.Nodes.Contains(minPair.Second)).ToArray();
+
+            if (circuitsWithFirst.Length > 1 || circuitsWithSecond.Length > 1)
+            {
+                throw new Exception("Something has gone wrong");
+            }
+
+            if (circuitsWithFirst.Length == 1 && circuitsWithSecond.Length == 1)
+            {
+                if (circuitsWithFirst[0] != circuitsWithSecond[0])
+                {
+                    // Combine circuits
+                    foreach (var node in circuitsWithSecond[0].Nodes)
+                    {
+                        circuitsWithFirst[0].Nodes.Add(node);
+                    }
+                    foreach (var connection in circuitsWithSecond[0].Connections)
+                    {
+                        circuitsWithFirst[0].Connections.Add(connection);
+                    }
+                    circuits.Remove(circuitsWithSecond[0]);
+                    latestConnection = newConnection;
+                }
+                else
+                {
+                    // Circuits are the same, no need to do anything
+                }
+            }
+            else if (circuitsWithFirst.Length == 1 && circuitsWithSecond.Length == 0)
+            {
+                circuitsWithFirst[0].Nodes.Add(minPair.Second);
+                circuitsWithFirst[0].Connections.Add(newConnection);
+                latestConnection = newConnection;
+            }
+            else if (circuitsWithFirst.Length == 0 && circuitsWithSecond.Length == 1)
+            {
+                circuitsWithSecond[0].Nodes.Add(minPair.First);
+                circuitsWithSecond[0].Connections.Add(newConnection);
+                latestConnection = newConnection;
+            }
+            else if (circuitsWithFirst.Length == 0 && circuitsWithSecond.Length == 0)
+            {
+                var newCircuit = new Circuit();
+                newCircuit.Connections.Add(newConnection);
+                newCircuit.Nodes.Add(minPair.First);
+                newCircuit.Nodes.Add(minPair.Second);
+                circuits.Add(newCircuit);
+                latestConnection = newConnection;
+            }
+        }
+
+        // Console.WriteLine($"Latest connection {latestConnection}");
+        var result = latestConnection!.First.X * latestConnection!.Second.X;
+
+        return result;
+    }
+
     private List<Vector3> ParseJunctionBoxes(List<string> input)
     {
         List<Vector3> results = [];
